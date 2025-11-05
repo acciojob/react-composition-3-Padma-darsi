6e1c9d7d746239ab45ad48a2d8db0e0c747c4498
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-11-06 01:00:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,20 +6,11 @@ const App = () => {
   return (
     <div id="main">
       {/* Do not remove the main div */}
-      <Tooltip text="This is a tooltip for Button">
+      <Tooltip text="This is tooltip text">
         <button>Hover over me</button>
-      </Tooltip>
-
-      <Tooltip text="Tooltip for text example">
-        <p>Hover over this paragraph</p>
-      </Tooltip>
-
-      <Tooltip text="Tooltip for link">
-        <a href="#">Hover over this link</a>
       </Tooltip>
     </div>
   );
 };
 
 export default App;
-
