75653ab73dca1c678da15b24873ba055e6243124
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-11-06 00:54:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-
 import React from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+    <div id="main">
+      {/* Do not remove the main div */}
+      <Tooltip text="This is a tooltip for Button">
+        <button>Hover over me</button>
+      </Tooltip>
 
-export default App
+      <Tooltip text="Tooltip for text example">
+        <p>Hover over this paragraph</p>
+      </Tooltip>
+
+      <Tooltip text="Tooltip for link">
+        <a href="#">Hover over this link</a>
+      </Tooltip>
+    </div>
+  );
+};
+
+export default App;
+
